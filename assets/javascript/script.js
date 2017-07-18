cd3a9f12463acb2c2c8339fda0fdc182f8f47169
Daniel Hernandez <daniel56hdz@gmail.com>
2017-07-18 03:41:26 -0500

    Added giphy api

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -23,14 +23,47 @@ function renderButtons() {
 	  	$("#buttons").append(a);
 	}
 }
+// Using the memeAPI to load our initial array
 $.get(memeAPI, function(res){
 	for (i=0;i<10;i++){
 		memes.push(res.data.memes[i].name);
 	}
 	renderButtons();
 });
+// displayMemeGiphys function re-renders the HTML to display the appropriate content
+function displayMemeGiphys() {
+	var meme = $(this).attr("data-name");
+	var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=b2ec119853c2410aa76aced2021ac552&q=" + meme + "&limit=10&offset=0&rating=PG-13&lang=en";
+	// Creates AJAX call for the specific meme button being clicked
+	$.ajax({
+		url: queryURL,
+	  	method: "GET"
+	}).done(function(response) {
+		console.log(response)
+		// var h1 = $('<h1/>');
+		// $('#movies-view').append(h1);
+		// $(h1).append(response.Title);
+		// var p1 = $('<p/>');
+		// $('#movies-view').append(p1);
+		// $(p1).append(response.Rated);
+		// var p2 = $('<p/>');
+		// $('#movies-view').append(p2);
+		// $(p2).append(response.Released);
+		// var p3 = $('<p/>');
+		// $('#movies-view').append(p3);
+		// $(p3).append(response.Plot);
+		// var img = $('<img/>');
+		// $('#movies-view').append(img);
+		// img.attr('200px');
+		// $(img).attr('src', response.Poster);
+	});
+}
 
 
+
+
+
+//Make the submit button perform a function
 $("#add-meme").on("click", function(event) {
 	event.preventDefault();
 	// This line of code will grab the input from the textbox
@@ -42,3 +75,5 @@ $("#add-meme").on("click", function(event) {
 	// Calling renderButtons which handles the processing of our meme array
 	renderButtons();
 });
+// Adding click event listeners to all elements with a class of "movie"
+$(document).on("click", ".meme", displayMemeGiphys);
