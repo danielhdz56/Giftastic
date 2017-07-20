5c69d5aaead5d9b8c17203d07e4cd0a274356ac0
Daniel Hernandez <daniel56hdz@gmail.com>
2017-07-19 20:32:08 -0500

    image sized appropriately

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -47,17 +47,40 @@ function displayMemeGiphys() {
 		url: queryURL,
 	  	method: "GET"
 	}).done(function(response) {
-		console.log(response);
-		for(i=0;i<response.data.length;i++){
+		if (response.data.length === 0) {
 			var column = $('<div>');
 			column.addClass('col-xs-12 col-sm-6');
 			$('#images').append(column);
-			var image = $('<img>');
-			image.addClass('m-0a still')
-			image.attr('src', response.data[i].images.original_still.url);
-			image.attr('data-still', response.data[i].images.original_still.url);
-			image.attr('data-gif', response.data[i].images.original.url);
-			$(column).append(image);
+			var paragraph1 = $('<p>');
+			paragraph1.append("Your search <strong>" + meme + " </strong> did not match any images.");
+			$(column).append(paragraph1);
+			var paragraph2 = $('<p>');
+			paragraph2.append("Suggestions:")
+			$(column).append(paragraph2);
+			var uList = $('<ul>');
+			var list1 = $('<li>');
+			list1.append("Make sure all words are spelled correctly.")
+			var list2 = $('<li>');
+			list2.append("Try different keywords.");
+			var list3 = $('<li>');
+			list3.append("Try more general keywords.");
+			var list4 = $('<li>');
+			list4.append("Try fewer keywords.");
+			uList.append(list1, list2, list3, list4);
+			$(column).append(uList);
+		}
+		else {
+			for(i=0;i<response.data.length;i++) {
+				var column = $('<div>');
+				column.addClass('col-xs-12 col-sm-6');
+				$('#images').append(column);
+				var image = $('<img>');
+				image.addClass('still')
+				image.attr('src', response.data[i].images.original_still.url);
+				image.attr('data-still', response.data[i].images.original_still.url);
+				image.attr('data-gif', response.data[i].images.original.url);
+				$(column).append(image);
+			}
 		}
 	});
 }
@@ -78,8 +101,12 @@ $("#add-meme").on("click", function(event) {
 	event.preventDefault();
 	// This line of code will grab the input from the textbox
 	var meme = $("#meme-input").val().trim();
+	//This makes sure that it isn't empty
+	if(meme === ""){
+		alert("You must enter a value");
+	}
 	// This makes sure that only unique values are added
-	if($.inArray(meme, memes) === -1) {
+	else if($.inArray(meme, memes) === -1) {
 		// The meme from the textbox is then added to our array
 		memes.push(meme);
 		console.log(memes);
@@ -92,6 +119,9 @@ $("#add-meme").on("click", function(event) {
 			});
 			autoComplete(unusedMemes);
 		}
+	}
+	else {
+		alert("You have already used this before")
 	}
 	// resets the input field
 	$("#form")[0].reset();
